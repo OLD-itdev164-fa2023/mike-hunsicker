8b357182e542b173cc45e41bb3bf72bfaea02df3
Mike Hunsicker <hunsickm@matc.edu>
2022-10-30 21:14:10 -0500

    Used IconButton in Header

--- blog/src/components/Button/BaseButton.js
+++ blog/src/components/Button/BaseButton.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Button } from 'styled-components'
+import { Button } from 'rebass'
+import styled from 'styled-components'
 
 const Reset = styled(Button)`
   background: none;
