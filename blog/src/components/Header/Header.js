c03e76850880c31f6c9f5b06ff11aabb3a84559c
Mike Hunsicker <hunsickm@matc.edu>
2022-10-30 22:08:18 -0500

    Completed Activity 8

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -9,7 +9,7 @@ import { Section } from '../Section'
 
 
 const Outer = styled.header`
-  background: ${props => props.theme.header.backgroundColor};
+  background: ${({ theme }) => theme.variants.header.primary.backgroundColor};
   margin-bottom: 1.45rem;
   margin: 0 auto;
   padding: var(--space-4) var(--size-gutter);
@@ -36,7 +36,7 @@ const Header = ({ siteTitle }) => (
       </H1>
     </Section>
     <Section width={1/12}>
-      <IconButton icon={<Search />} />
+      <IconButton icon={<Search />} variant='contrast' />
     </Section>
   </Outer>
 )
