16f1875a28e87dc6342abbf5de6ebceca5cd167f
Mike Hunsicker <hunsickm@matc.edu>
2023-03-22 11:02:15 -0500

    Small change to trigger a build and test Netlify build and deploy process

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -4,7 +4,7 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `House of Vinyl`,
+    title: `Random Blog`,
     description: `ITDEV-164 Gatsby Blog.`,
     author: `Mike Hunsicker`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
